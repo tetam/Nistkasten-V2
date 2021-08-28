c51838079432676054c00c66725ee4cfaf85176a
tetam <3255359+tetam@users.noreply.github.com>
2021-08-28 18:06:51 +0200

    Update installation.sh

--- Software/installation.sh
+++ Software/installation.sh
@@ -1,1 +1,14 @@
-
+#!/bin/bash
+clear
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install motion ffmpeg v4l-utils python-pip python3-pip python-dev curl libssl-dev libcurl4-openssl-dev libjpeg-dev -y
+sudo pip3 install motioneye
+sudo mkdir -p /etc/motioneye
+sudo cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
+sudo mkdir -p /var/lib/motioneye
+sudo cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
+sudo systemctl daemon-reload
+sudo systemctl enable motioneye
+sudo systemctl start motioneye
